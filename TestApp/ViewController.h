bd3ee8d33e2d781a66669df3f53d76f650ef9ca5
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2013-11-26 17:46:18 +0900

    Initial Commit

---
+++ TestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by 田中 武則2 on 2013/11/26.
+//  Copyright (c) 2013年 takenori tanaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by 田中 武則2 on 2013/11/26.
+//  Copyright (c) 2013年 takenori tanaka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
